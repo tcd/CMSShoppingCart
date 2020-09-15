8e0e8c61f4cae715c128a53a5955f3807926f22b
Clay Dunston <clay.dunston@askFTA.com>
2020-09-15 15:09:45 -0500

    Add view & controller code for Pages admin

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CMSShoppingCart.Infrastructure;
+using CMSShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMSShoppingCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly CmsShoppingCartContext context;
+
+        public PagesController(CmsShoppingCartContext context)
         {
-            return "test response";
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }
