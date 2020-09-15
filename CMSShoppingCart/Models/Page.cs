f3bf57fd4fdd3e2c26b31aa0e40bbc15ad5988a8
Clay Dunston <clay.dunston@askFTA.com>
2020-09-15 13:30:32 -0500

    Initial migrations; Create DB

--- CMSShoppingCart/Models/Page.cs
+++ CMSShoppingCart/Models/Page.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace CMSShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Slug { get; set; }
+        [Required]
         public string Content { get; set; }
         public int Sorting { get; set; }
     }
