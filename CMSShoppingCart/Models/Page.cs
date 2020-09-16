b5c17a84de43c4ad4090f8c47532ed5c67e14ba6
Clay Dunston <clay.dunston@askFTA.com>
2020-09-16 15:11:34 -0500

    Add validations to `Page` model

--- CMSShoppingCart/Models/Page.cs
+++ CMSShoppingCart/Models/Page.cs
@@ -9,11 +9,17 @@ namespace CMSShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required]
+        [
+            Required,
+            MinLength(2, ErrorMessage = "Minimum length is 2"),
+        ]
         public string Title { get; set; }
         [Required]
         public string Slug { get; set; }
-        [Required]
+        [
+             Required,
+             MinLength(4, ErrorMessage = "Minimum length is 4"),
+         ]
         public string Content { get; set; }
         public int Sorting { get; set; }
     }
