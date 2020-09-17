ca068bd5c4c1e077916e63a29230b0f49f6fc46d
Clay Dunston <clay.dunston@askFTA.com>
2020-09-17 12:25:47 -0500

    Add page editing

--- CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -54,7 +54,10 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
         /// <summary>
         /// POST /admin/pages/create
         /// </summary>
-        [HttpPost]
+        [
+            HttpPost,
+            ValidateAntiForgeryToken,
+        ]
         public async Task<IActionResult> Create(Page page)
         {
             if (ModelState.IsValid)
@@ -75,10 +78,74 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
                 TempData["Success"] = $"New page '{page.Title}' has been added.";
 
 
-                return RedirectToAction("Index", "Pages");
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                string messages = string.Join("; ", ModelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+                ModelState.AddModelError("", messages);
             }
 
             return View(page);
         }
+
+        /// <summary>
+        /// GET /admin/pages/edit/{id}
+        /// </summary>
+        /// <param name="id">Primary key of the record to edit</param>
+        public async Task<IActionResult> Edit(int id)
+        {
+            Page page = await context.Pages.FindAsync(id);
+            if (page == null)  
+            {
+                return NotFound();
+            }
+            return View(page);
+        }
+
+        /// <summary>
+        /// POST /admin/pages/create
+        /// </summary>
+        [
+            HttpPost,
+            ValidateAntiForgeryToken,
+        ]
+        public async Task<IActionResult> Edit(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = (page.Id == 1) ? "home" : page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
+
+                var slug = await context.Pages
+                                        .Where(x => x.Id != page.Id)
+                                        .FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "A Page with this title already exists.");
+                    return View(page);
+                }
+
+                context.Update(page);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = $"Page '{page.Title}' has been updated.";
+
+
+                return RedirectToAction("Edit", new { id = page.Id });
+            }
+            else
+            {
+                string messages = string.Join("; ", ModelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+                ModelState.AddModelError("", messages);
+            }
+
+            return View(page);
+        }
+
     }
 }
