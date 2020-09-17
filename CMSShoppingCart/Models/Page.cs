fdd416608f6f1cffd694473d61b29f94b7a7a467
Clay Dunston <clay.dunston@askFTA.com>
2020-09-17 12:26:26 -0500

    Update `Page` model

--- CMSShoppingCart/Models/Page.cs
+++ CMSShoppingCart/Models/Page.cs
@@ -8,18 +8,18 @@ namespace CMSShoppingCart.Models
 {
     public class Page
     {
-        public int Id { get; set; }
+        public int Id      { get; set; }
+        public int Sorting { get; set; }
+        public string Slug { get; set; }
         [
             Required,
             MinLength(2, ErrorMessage = "Minimum length is 2"),
         ]
         public string Title { get; set; }
-        public string Slug { get; set; }
         [
              Required,
              MinLength(4, ErrorMessage = "Minimum length is 4"),
-         ]
+        ]
         public string Content { get; set; }
-        public int Sorting { get; set; }
     }
 }
