06819d207dfc47bd3230409a3a73a30a095ee871
Clay Dunston <clay.dunston@askFTA.com>
2020-09-22 12:34:58 -0500

    Add admin create action & view for `Category`

--- CMSShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +25,54 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await context.Categories.OrderBy(x => x.Sorting).ToListAsync());
+            // IQueryable<Category> categories = from c in context.Categories orderby c.Sorting select c;
+            // List<Category> categoriesList = await categories.ToListAsync();
+            // return View(categoriesList);
+        }
+
+        /// <summary>
+        /// GET /admin/categories/create
+        /// </summary>
+        public IActionResult Create() => View();
+
+
+        /// <summary>
+        /// POST /admin/categories/create
+        /// </summary>
+        [
+            HttpPost,
+            ValidateAntiForgeryToken,
+        ]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+                category.Sorting = 100;
+
+                var slug = await context.Categories.FirstOrDefaultAsync(x => x.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "A Category with this name already exists.");
+                    return View(category);
+                }
+
+                context.Add(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = $"New category '{category.Name}' has been added.";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                string messages = string.Join("; ", ModelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+                ModelState.AddModelError("", messages);
+            }
+
+            return View(category);
         }
 
     }
