8fb73ec011cd53d78f4e812cd4860fe72d2c949a
Clay Dunston <clay.dunston@askFTA.com>
2020-09-22 14:06:04 -0500

    Add edit, delete, and reorder code for `Category`

--- CMSShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
+++ CMSShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -25,15 +25,15 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await context.Categories.OrderBy(x => x.Sorting).ToListAsync());
-            // IQueryable<Category> categories = from c in context.Categories orderby c.Sorting select c;
-            // List<Category> categoriesList = await categories.ToListAsync();
-            // return View(categoriesList);
         }
 
         /// <summary>
         /// GET /admin/categories/create
         /// </summary>
-        public IActionResult Create() => View();
+        public IActionResult Create() 
+        {
+            return View();
+        } 
 
 
         /// <summary>
@@ -73,6 +73,100 @@ namespace CMSShoppingCart.Areas.Admin.Controllers
             }
 
             return View(category);
+        }
+
+        /// <summary>
+        /// GET /admin/categories/edit/{id}
+        /// </summary>
+        /// <param name="id">Primary key of the record to edit</param>
+        public async Task<IActionResult> Edit(int id)
+        {
+            Category category = await context.Categories.FindAsync(id);
+            if (category == null)  
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        /// <summary>
+        /// POST /admin/categories/edit/{id}
+        /// </summary>
+        /// <param name="id">Primary key of the record to edit</param>
+        /// <param name="category">Form data used to edit the record</param>
+        [
+            HttpPost,
+            ValidateAntiForgeryToken,
+        ]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Categories
+                                        .Where(x => x.Id != id)
+                                        .FirstOrDefaultAsync(x => x.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "A Category with this name already exists.");
+                    return View(category);
+                }
+
+                context.Update(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = $"Category '{category.Name}' has been updated.";
+
+                // NOTE: Implied key names like JS 🤔
+                return RedirectToAction("Edit", new { id });
+            }
+            else
+
+            return View(category);
+        }
+
+        /// <summary>
+        /// GET /admin/categories/delete/{id}
+        /// </summary>
+        /// <param name="id">Primary key of the record to delete</param>
+        public async Task<IActionResult> Delete(int id)
+        {
+            Category category = await context.Categories.FindAsync(id);
+            if (category == null)  
+            {
+                TempData["Error"] = "You can not delete that which does not exist <br/> - the developer";
+            }
+            else 
+            {
+                context.Categories.Remove(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = $"Category '{category.Name}' has been deleted";
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// POST /admin/categories/reorder
+        /// </summary>
+        /// <param name="id">Form data containing new order data</param>
+        [HttpPost]
+        public async Task<IActionResult> Reorder(int[] id)
+        {
+            int count = 1;
+
+            foreach (var categoryId in id)
+            {
+                Category category = await context.Categories.FindAsync(categoryId);
+                category.Sorting = count;
+                context.Update(category);
+                await context.SaveChangesAsync();
+                count++;
+            }
+
+            return Ok();
         }
 
     }
