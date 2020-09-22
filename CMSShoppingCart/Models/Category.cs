accdea10ca60559c490db1171d3740bcc03f788d
Clay Dunston <clay.dunston@askFTA.com>
2020-09-22 14:44:28 -0500

    Updates to existing models.

--- CMSShoppingCart/Models/Category.cs
+++ CMSShoppingCart/Models/Category.cs
@@ -8,14 +8,22 @@ namespace CMSShoppingCart.Models
 {
     public class Category
     {
-        public int Id      { get; set; }
+
+        public int Id { get; set; }
+
         public int Sorting { get; set; }
+
+        [
+            Required(ErrorMessage = "The Slug field is required"),
+        ]
         public string Slug { get; set; }
+
         [
             Required(ErrorMessage = "The Name field is required"),
             MinLength(2, ErrorMessage = "Minimum length is 2"),
-            RegularExpression(@"^[a-zA-Z-]*$", ErrorMessage = "Only letters and dashes are allowed")
+            RegularExpression(@"^[a-zA-Z-]*$", ErrorMessage = "Only letters and dashes are allowed"),
         ]
         public string Name { get; set; }
+
     }
 }
--- CMSShoppingCart/Models/Page.cs
+++ CMSShoppingCart/Models/Page.cs
@@ -8,19 +8,25 @@ namespace CMSShoppingCart.Models
 {
     public class Page
     {
+        
         public int Id      { get; set; }
+
         public int Sorting { get; set; }
+
         public string Slug { get; set; }
+
         [
             Required(ErrorMessage = "The Title field is required"),
             MinLength(2, ErrorMessage = "Minimum length is 2"),
             RegularExpression(@"^[a-zA-Z-]*$", ErrorMessage = "Only letters and dashes are allowed"),
         ]
         public string Title { get; set; }
+
         [
             Required(ErrorMessage = "The Content field is required"),
             MinLength(4, ErrorMessage = "Minimum length is 4"),
         ]
         public string Content { get; set; }
+
     }
 }
