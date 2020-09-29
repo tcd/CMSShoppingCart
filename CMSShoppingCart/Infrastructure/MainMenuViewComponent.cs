34ab843abac00fc88b5912fdc36a1e79b59d3b3f
Clay Dunston <claydunston@gmail.com>
2020-09-29 12:09:24 -0500

    Add `MainMenuComponent`

---
+++ CMSShoppingCart/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CMSShoppingCart.Models;
+
+namespace CMSShoppingCart.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly CMSShoppingCartContext context;
+
+        public MainMenuViewComponent(CMSShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
